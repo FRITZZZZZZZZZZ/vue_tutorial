0dce4bc9461d1a0bdcf77849af775d2bfe6690d5
Fritz <friedrie07@zedat.fu-berlin.de>
2025-12-01 15:48:48 +0100

    Fixed styling

--- vue_start_spa/src/App.vue
+++ vue_start_spa/src/App.vue
@@ -1,31 +1,46 @@
 <template>
-    <p>simple text</p>
+    <navbar
+    :pages="pages"
+    :background-color="backgroundColor"
+    :active-page="activePage"
+    :nav-link-click="(index) => activePage = index"
+    ></navbar>
+    
+    <page-viewer 
+    v-show="pages.length > 0"
+    :page="pages[activePage]">
+    </page-viewer>
 </template>
 
 <script>
+import NavBarLink from './components/NavBarLink.vue'
+
+import Navbar from './components/Navbar.vue';
+
+import PageViewer from './components/PageViewer.vue';
+
 export default {
+    components: {
+        Navbar,
+        PageViewer
+    },
+    created() {
+        this.getPages();
+    },
     data() {
         return {
             activePage: 0,
-            pages: [
-               {
-                    link: {text: 'Home', url: 'index.html'},
-                    pageTitle: 'Home Page', 
-                    content: 'This is the home content'
-                },
-                {
-                    link: {text: 'About', url: 'abou.html'}, 
-                    pageTitle: 'About Page', 
-                    content: 'This is the about content'
-                },
-                {
-                    link: {text: 'Contact', url: 'contact.html'},
-                    pageTitle: 'Contact Page',
-                    content: 'This is the contact content'
-                }
-            ]
+            pages: []
         };
     },
+    methods: {
+        async getPages() {
+            let res = await fetch('pages.json');
+            let data = await res.json();
+
+            this.pages = data;
+        }
+    }
 }
 </script>
 <style>
---
+++ vue_start_spa/src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav :style="{backgroundColor}">
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand">my vue</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li v-for="(page, index) in pages" class="nav-item" :key="index">
+                    <nav-bar-link 
+                        :page="page"
+                        :isActive="activePage == index"
+                        @click.prevent="navLinkClick(index)"
+                    ></nav-bar-link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+                    
+    <form>
+        <button
+        @click.prevent="toggleBackground()">toggle navbar </button>
+    </form>
+</template>
+
+<script>
+    import NavBarLink from './NavBarLink.vue';
+    export default {
+        components: {
+            NavBarLink
+        },
+        props: ['backgroundColor', 'pages', 'activePage', 'navLinkClick'],
+        data () {
+            return {
+                backgroundColor: 'blue'
+            };
+        },
+        methods: {
+            toggleBackground () {
+                if (this.backgroundColor == "blue") {
+                        this.backgroundColor = "red";
+                } else {
+                    this.backgroundColor = "blue";
+                }
+            }
+        }
+    };
+</script>
